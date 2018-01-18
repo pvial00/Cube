a65bbb183a92074799e2c68998502c70c35af2ac
Karl <pvial@krypt.local>
2018-01-17 22:05:27 -0800

    Cube v0.5.2

--- cubecrypt.cpp
+++ cubecrypt.cpp
@@ -4,50 +4,55 @@
 #include <fstream>
 #include <utility>
 #include <algorithm>
-#include "cubekdf.h"
+#include "cubeclass.h"
 
-int iterations = 2;
-int keylen = 16;
+int iterations = 10;
+int keylen = 128;
+int nonce_length = 16;
 
 using namespace std;
 
 int main(int argc, char** argv) {
     ifstream infile;
     ofstream outfile;
-    string mode;
-    string data;
-    string in;
-    string out;
-    string key;
+    string mode, in, out, key, msg, nonce, data;
     unsigned char b;
     mode = argv[1];
     in = argv[2];
     out = argv[3];
     key = argv[4];
-    int chk;
+    string random_dev = "/dev/urandom";
+    int i;
     infile.open(in.c_str(), std::ios::binary);
-    while (!infile.eof()) {
-	    b = infile.get();
-            chk = infile.peek();
-	    if (chk == EOF) {
-                if (infile.eof()) {
-		    break;
-		}
-            }
-	    else {
-	        data.append(1, b);
-	    }
+    infile.seekg(0, ios::end);
+    int fsize = infile.tellg();
+    infile.close();
+    infile.open(in.c_str(), std::ios::binary);
+    for (i = 0; i < fsize; i++) {
+        b = infile.get();
+	data.push_back(b);
     }
     infile.close();
     string c;
-    key = genkey(key, keylen, iterations);
+    CubeKDF kdf;
+    key = kdf.genkey(key, keylen, iterations);
+    Cube cube;
     if (mode == "encrypt") {
-    	c = cube_encrypt(data, key);
+	CubeRandom rand;
+	nonce = rand.random(nonce_length);
+    	c = cube.encrypt(data, key, nonce);
+        outfile.open(out.c_str());
+        outfile << nonce;
+        outfile << c;
+	outfile.close();
     }
     else if (mode == "decrypt") {
-	c = cube_decrypt(data, key);
+	nonce = data.substr(0, nonce_length);
+	msg = data.substr(nonce_length, (data.length() - nonce_length));
+	c = cube.decrypt(msg, key, nonce);
+        outfile.open(out.c_str());
+        outfile << c;
+        outfile.close();
     }
-    outfile.open(out.c_str());
-    outfile << c;
-    outfile.close();
+    return 0;
 }
