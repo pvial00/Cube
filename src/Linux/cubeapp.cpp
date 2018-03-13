a94d226e2cdb2d15f0a4fa20917a80b734b9895e
Karl <pvial@krypt.local>
2018-03-12 22:51:54 -0700

    Cube v1.0

---
+++ src/Linux/cubeapp.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <utility>
+#include <algorithm>
+#include "cubecrypt.cpp"
+#include <openssl/hmac.h>
+
+int iterations = 10;
+int keylen = 16;
+int nonce_length = 16;
+int mac_length = 32;
+int buffersize = 100;
+int hashlen = 16;
+int numbytes = 1;
+int seedlength = keylen;
+int keylength = 16;
+
+void desc() {
+    cout << "Cube v1.0 *** Cube ciphering machine ***\n\n";
+    cout << "Warning: This machine is designed to work as is, without modification.\n";
+    cout << "Use at your at own risk!\n";
+}
+void usage() {
+    desc();
+    cout << "Author: pvial@kryptomagik.com\n\n";
+    cout << "-e <input file> <output file> <password> (Encrypt)\n";
+    cout << "-d <input file> <output file> <password> (Decrypt)\n";
+    cout << "-emac <input file> <output file> <password> (Encrypt with HMAC)\n";
+    cout << "-dmac <input file> <output file> <password> (Decrypt with HMAC)\n";
+    cout << "-sum <input file>\n";
+    cout << "-kdf <password> <optional keylength in bytes> <optional # of iterations>\n";
+    cout << "-random <optional number of bytes default is 1 byte>\n";
+}
+void kdfusage() {
+    desc();
+    cout << "Author: pvial@kryptomagik.com\n";
+    cout << "Usage: cubecrypt <encrypt/decrypt> <input file> <output file> <password>" << "\n";
+}
+void sumusage() {
+    desc();
+    cout << "Author: pvial@kryptomagik.com\n";
+    cout << "Usage: cubecrypt <encrypt/decrypt> <input file> <output file> <password>" << "\n";
+}
+
+void file_missing() {
+    cout << "Error: input file missing.\n";
+}
+
+int main(int argc, char** argv) {
+    ifstream infile;
+    ofstream outfile;
+    string in, out, key, msg, nonce, data, mode;
+    unsigned char b;
+    int i;
+    string c;
+    unsigned char *mac;
+    CubeKDF kdf;
+    CubeSum cubesum;
+    CubeCrypt cube;
+    CubeRandom rand;
+    int s;
+    if (argc >= 2) {
+        mode = argv[1];
+	if (!isatty(STDIN_FILENO)) {
+	    key = argv[2];
+	    in = "0";
+	    out = "0";
+	}
+    }
+    else {
+        usage();
+	exit(EXIT_FAILURE);
+    }
+
+    if (mode == "-e") {
+	if (isatty(STDIN_FILENO)) {
+            if (argc < 5) {
+                usage();
+	        exit(EXIT_FAILURE);
+            }
+            in = argv[2];
+            out = argv[3];
+            key = argv[4];
+        }
+        key = kdf.genkey(key, keylen, iterations);
+	nonce = rand.random(nonce_length, nonce_length, nonce_length, iterations);
+    	cube.encrypt(in, out, key, nonce, buffersize);
+        return 0;
+    }
+    else if (mode == "-emac") {
+        if (argc < 5) {
+            usage();
+	    exit(EXIT_FAILURE);
+        }
+        in = argv[2];
+        out = argv[3];
+        key = argv[4];
+        key = kdf.genkey(key, keylen, iterations);
+	nonce = rand.random(nonce_length, nonce_length, nonce_length, iterations);
+    	cube.encrypt(in, out, key, nonce, buffersize);
+        infile.open(out.c_str(), std::ios::binary);
+	if(infile.is_open()) {
+            infile.seekg(0, ios::end);
+            int fsize = infile.tellg();
+            infile.seekg(0, ios::beg);
+	    for (int i = 0; i < fsize; i++) {
+	        b = infile.get();
+		data.push_back(b);
+	    }
+            infile.close();
+	    mac = HMAC(EVP_sha256(), key.c_str(), key.length(), reinterpret_cast<const unsigned char *>(data.c_str()), data.length(), NULL, NULL);
+	    string mm(reinterpret_cast<char*>(mac));
+            outfile.open(out.c_str(), std::ios::app|std::ios::binary);
+            outfile << mm;
+	    outfile.close();
+	}
+	else {
+            file_missing();
+	    exit(EXIT_FAILURE);
+	}
+    }
+    else if (mode == "-d") {
+        /*if (argc < 5) {
+            usage();
+	    exit(EXIT_FAILURE);
+        }*/
+	if (isatty(STDIN_FILENO)) {
+            in = argv[2];
+            out = argv[3];
+            key = argv[4];
+	}
+        key = kdf.genkey(key, keylen, iterations);
+	if (isatty(STDIN_FILENO)) {
+            infile.open(in.c_str(), std::ios::binary);
+	    if(infile.is_open()) {
+                infile.seekg(0, ios::end);
+                int fsize = infile.tellg();
+	        infile.close();
+	        if (fsize < (nonce_length + 1)) {
+	            cout << "Error:  File size failure\n";
+	            exit(2);
+                }
+	    }
+	    else {
+		file_missing();
+		exit(EXIT_FAILURE);
+	    }
+	}
+	cube.decrypt(in, out, key, nonce_length, buffersize);
+	return 0;
+    }
+    else if (mode == "-dmac") {
+        if (argc < 5) {
+            usage();
+	    exit(EXIT_FAILURE);
+        }
+        in = argv[2];
+        out = argv[3];
+        key = argv[4];
+        key = kdf.genkey(key, keylen, iterations);
+        infile.open(in.c_str(), std::ios::binary);
+	if(infile.is_open()) {
+            infile.seekg(0, ios::end);
+            int fsize = infile.tellg();
+            infile.seekg(0, ios::beg);
+	    if (fsize < (nonce_length + mac_length + 1)) {
+	        cout << "Error:  File size failure\n";
+	        exit(2);
+	    }
+	    for (int i = 0; i < fsize; i++) {
+	        b = infile.get();
+		data.push_back(b);
+	    }
+	    infile.close();
+	    string m, ctxt;
+	    m = data.substr((data.length() - mac_length), mac_length);
+	    ctxt = data.substr(0, (data.length() - mac_length));
+	    data.clear();
+	    mac = HMAC(EVP_sha256(), key.c_str(), key.length(), reinterpret_cast<const unsigned char *>(ctxt.c_str()), ctxt.length(), NULL, NULL);
+	    string m2(reinterpret_cast<char*>(mac));
+	    if (m2.compare(m) == 0) {
+		outfile.open(in.c_str(), std::ios::binary);
+		outfile << ctxt;
+		outfile.close();
+	        cube.decrypt(in, out, key, nonce_length, buffersize);
+	    }
+	    else {
+                cout << "MAC failed: message has been tampered with." << "\n";
+	        exit(1);
+	    }
+	}
+	else {
+	    file_missing();
+	    exit(EXIT_FAILURE);
+	}
+    }
+    else if(mode == "-kdf") {
+        if (argc < 3) {
+            kdfusage();
+	    exit(EXIT_FAILURE);
+        }
+
+	if(argc >= 3) {
+            key = argv[2];
+	}
+	if(argc >= 4) {
+	    keylen = atoi(argv[3]);
+	}
+	if(argc >= 5) {
+	    iterations = atoi(argv[4]);
+	}
+	key = kdf.genkey(key, keylen, iterations);
+	for (unsigned char k: key) {
+	    cout << std::hex << static_cast<unsigned int>(k);
+	}
+	cout << "\n";
+	return 0;
+    }
+    else if(mode == "-sum") {
+	string digest;
+	int i;
+	unsigned char b;
+	if (argc < 3) {
+	    sumusage();
+	    exit(EXIT_FAILURE);
+	}
+	else {
+	    in = argv[2];
+	}
+	if (argc >= 4) {
+	    hashlen = atoi(argv[3]);
+	}
+	infile.open(in.c_str(), std::ios::binary);
+	if(infile.is_open()) {
+	    infile.seekg(0, ios::end);
+	    int fsize = infile.tellg();
+	    infile.close();
+	    infile.open(in.c_str(), std::ios::binary);
+	    for (int i = 0; i < fsize; i++) {
+	        b = infile.get();
+	        data.push_back(b);
+	    }
+	    infile.close();
+	    digest = cubesum.digest(data, string(), hashlen);
+	    cout << in << ": ";
+	    for (unsigned char d: digest) {
+	        cout << std::hex << static_cast<unsigned int>(d);
+	    }
+	    cout << "\n";
+	    return 0;
+	}
+	else {
+	    file_missing();
+	    exit(EXIT_FAILURE);
+	}
+    }
+    else if(mode == "-random") {
+	if (argc == 3) {
+	    numbytes = atoi(argv[2]);
+	}
+	else {
+            numbytes = 1;
+	}
+	data = rand.random(numbytes, seedlength, keylen, iterations);
+	for (unsigned char b: data) {
+	    cout << b;
+	}
+	return 0;
+    }
+}
---
+++ src/Linux/cubecrypt.cpp
@@ -0,0 +1,331 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <utility>
+#include <algorithm>
+#include <unistd.h>
+#include "cube.cpp"
+
+using namespace std;
+
+class CubeCrypt {
+    public:
+    int size_factor = 3;
+    int alphabet_size = 256;
+    vector< vector< vector<int> > > state;
+    ifstream infile;
+    ofstream outfile;
+    int max_stdin_buf = 500000000;
+
+    void gen_cube (int depth, int width, int length) {
+        for (int z=0; z < depth; z++) {
+            vector< vector<int> > section;
+            for (int y=0; y < width; y++) {
+                vector<int> alphabet;
+                for (int x=0; x < length; x++) {
+                    alphabet.push_back(x);
+                }
+                for (int mod=0; mod < y; mod++) {
+    		    int shift;
+                    shift = alphabet.at(0);
+		    alphabet.erase(alphabet.begin()+0);
+                    alphabet.push_back(shift);
+                    shift = alphabet.at(2);
+                    alphabet.erase(alphabet.begin()+2);
+                    alphabet.insert(alphabet.begin()+127,shift);
+                }
+                section.push_back(alphabet);
+            }
+            state.push_back(section);
+        }
+    }
+
+    void key_cube (string key) {
+        int z;
+        int x;
+        int y;
+        int k;
+        int shuffle;
+        int key_sub;
+        int sized_pos;
+        for (z=0; z < state.size(); z++) {
+            for (unsigned char k: key) {
+                for (x=0; x < state[z].size(); x++) {
+                    key_sub = state[z][x].at(int(k));
+		    state[z][x].erase(state[z][x].begin()+int(k));
+                    state[z][x].push_back(key_sub);
+                    for (y=0; y < int(k); y++) {
+                        if (y % 2 == 0) {
+                            shuffle = state[z][x].at(0);
+                            state[z][x].erase(state[z][x].begin()+0);
+                            state[z][x].push_back(shuffle);
+                            shuffle = state[z][x].at(2);
+                            state[z][x].erase(state[z][x].begin()+2);
+                            state[z][x].insert(state[z][x].begin()+127,shuffle);
+                        }
+                    }
+                }
+            }
+        }
+        vector< vector<int> > section;
+        for (unsigned char k: key) {
+	    sized_pos = int(k) % size_factor;
+	    for (y=0; y < int(k); y++) {
+		    section = state.at(sized_pos);
+		    state.erase(state.begin()+sized_pos);
+		    state.push_back(section);
+            }
+        }
+    }
+
+    string key_scheduler (string key) {
+        int x;
+        int sized_pos;
+        int sub;
+        vector< vector<int> > section;
+        vector<int> sub_alpha;
+        string sub_key;
+        for (unsigned char k: key) {
+	    sized_pos = int(k) % size_factor;
+            sub = state[sized_pos][sized_pos][int(k)];
+	    state[sized_pos][sized_pos].erase(state[sized_pos][sized_pos].begin()+int(k));
+            state[sized_pos][sized_pos].push_back(sub); 
+	    sub_key.push_back(char(sub));
+        }
+        return sub_key;
+    }
+
+    void morph_cube (int counter, string k) {
+        int mod_value;
+        int shift;
+        int z;
+        int y;
+        int ke;
+        vector< vector<int> >  section_shift;
+        mod_value = counter % alphabet_size;
+        for (z=0; z < state.size(); z++) {
+	    for (unsigned char key_element : k) {
+                for (y=0; y < state[z].size(); y++) {
+		    swap(state[z][y][mod_value], state[z][y][int(key_element)]);
+		    ke = key_element;
+                }
+	    }
+            shift = int(ke) % size_factor;
+            section_shift = state.at(shift);
+	    state.erase(state.begin()+shift);
+	    state.push_back(section_shift);
+        }
+    }
+
+    void encrypt (string input, string output, string key, string nonce = string(), int blocksize = 100) {
+	int fsize = 0;
+	int stdin_waiting = 0;
+	if (!isatty(STDIN_FILENO)) {
+	    cout << nonce;
+	    fsize = max_stdin_buf;
+	    stdin_waiting = 1;
+	}
+	else {
+            infile.open(input.c_str(), std::ios::binary);
+	    if(infile.is_open()) {
+                infile.seekg(0, ios::end);
+		fsize = infile.tellg();
+                infile.close();
+                infile.open(input.c_str(), std::ios::binary);
+                outfile.open(output.c_str(), std::ios::binary);
+                outfile << nonce;
+	    }
+	    else {
+                cout << "Error: Input file is missing.\n";
+		exit(EXIT_FAILURE);
+	    }
+	}
+        gen_cube(size_factor, size_factor, alphabet_size);
+        key_cube(key);
+        key_cube(nonce);
+        int ctr = 0;
+        int sub, sub_pos, shift, z, y;
+        string sub_key, ctxt;
+        sub_key = key;
+        char buf[blocksize];
+        int extrablock = 0;
+        int extra = 0;
+        int blocks = 0;
+        if (fsize <= blocksize) {
+            blocks = 1;
+            blocksize = fsize;
+        }
+        else {
+            blocks = (fsize / blocksize);
+            if ((fsize % blocksize) != 0) {
+                extra = fsize % blocksize;
+                blocks++;
+            }
+        }
+	if (stdin_waiting == 0) {
+            for (int b = 0; b < blocks; b++) {
+                if ((extra > 0) && (b == (blocks - 1))) {
+                    blocksize = extra;
+                }
+	        if (stdin_waiting == 0) {
+                    infile.read(buf, blocksize);
+                }
+                for (unsigned char byte: buf) {
+                    sub = byte;
+	            for (z=0; z < state.size(); z++) {
+	                for (y=0; y < state[z].size(); y++) {
+                            sub_pos = sub;
+		            sub = state[z][y].at(sub_pos);
+		            shift = state[z][y].at(0);
+		            state[z][y].erase(state[z][y].begin()+0);
+		            state[z][y].push_back(shift);
+	                }
+	            }
+	            sub_key = key_scheduler(sub_key);
+	            morph_cube(ctr, sub_key);
+                    ctxt.push_back(char(sub));
+                    ctr++;
+                }
+                outfile << ctxt;
+                ctxt.clear();
+            } 
+        infile.close();
+        outfile.close();
+	}
+	else if (stdin_waiting == 1) {
+	    blocksize = 1;
+            while (cin) {
+	        if (cin) {
+	            cin.read(buf, blocksize);
+                    for (unsigned char byte: buf) {
+                        sub = byte;
+	                for (z=0; z < state.size(); z++) {
+	                    for (y=0; y < state[z].size(); y++) {
+                                sub_pos = sub;
+		                sub = state[z][y].at(sub_pos);
+		                shift = state[z][y].at(0);
+		                state[z][y].erase(state[z][y].begin()+0);
+		                state[z][y].push_back(shift);
+	                    }
+	                }
+	                sub_key = key_scheduler(sub_key);
+	                morph_cube(ctr, sub_key);
+                        ctxt.push_back(char(sub));
+                        ctr++;
+                    }
+		    if (!cin.eof()) {
+	                cout << ctxt;
+	            }
+                    ctxt.clear();
+		}
+            }
+        }
+    }
+
+   void decrypt (string input, string output, string key, int nonce_length = 16, int blocksize = 100) {
+	int fsize = 0;
+	int stdin_waiting = 0;
+        char * nonce_buf = new char [nonce_length];
+	if (!isatty(STDIN_FILENO)) {
+	    fsize = max_stdin_buf;
+	    cin.read(nonce_buf, nonce_length);
+	    stdin_waiting = 1;
+	}
+	else {
+            infile.open(input.c_str(), std::ios::binary);
+	    if (infile.is_open()) {
+                infile.seekg(0, ios::end);
+                fsize = infile.tellg();
+                fsize = fsize - nonce_length;
+                infile.close();
+                infile.open(input.c_str(), std::ios::binary);
+                infile.read(nonce_buf, nonce_length);
+                outfile.open(output.c_str(), std::ios::binary);
+	    }
+	    else {
+                cout << "Error: Input file missing.";
+		exit(EXIT_FAILURE);
+	    }
+	}
+        gen_cube(size_factor, size_factor, alphabet_size);
+        key_cube(key);
+	string nonce(nonce_buf);
+	key_cube(nonce);
+        int ctr = 0;
+        int sub, shift, sub_pos, z, y;
+        string sub_key, ptxt;
+        sub_key = key;
+        char buf[blocksize];
+        int extrablock = 0;
+        int extra = 0;
+        int blocks = 0;
+        if (fsize <= blocksize) {
+            blocks = 1;
+            blocksize = fsize;
+        }
+        else {
+            blocks = (fsize / blocksize);
+            if ((fsize % blocksize) != 0) {
+                blocks++;
+                extra = fsize % blocksize;
+            }
+        }
+	if (stdin_waiting == 0) {
+            for (int b = 0; b < blocks; b++) {
+                if ((extra > 0) && (b == (blocks - 1))) {
+                    blocksize = extra;
+                }
+	        if (stdin_waiting == 0) {
+                    infile.read(buf, blocksize);
+                }
+                for (unsigned char byte: buf) {
+                    sub = byte;
+	            for (z=state.size(); z--> 0;) {
+	                for (y=state[z].size(); y --> 0;) {
+		            sub = find(state[z][y].begin(), state[z][y].end(), sub) - state[z][y].begin();
+		            shift = state[z][y].at(0);
+		            state[z][y].erase(state[z][y].begin()+0);
+		            state[z][y].push_back(shift);
+	                }
+	            }
+	            sub_key = key_scheduler(sub_key);
+	            morph_cube(ctr, sub_key);
+                    ptxt.push_back(char(sub));
+                    ctr++;
+                }
+                outfile << ptxt;
+                ptxt.clear();
+            }
+            infile.close();
+            outfile.close();
+        }
+	else if (stdin_waiting == 1) {
+	    blocksize = 1;
+            while (cin) {
+		if (cin) {
+                    cin.read(buf, blocksize);
+                    for (unsigned char byte: buf) {
+                        sub = byte;
+	                for (z=state.size(); z--> 0;) {
+	                    for (y=state[z].size(); y --> 0;) {
+		                sub = find(state[z][y].begin(), state[z][y].end(), sub) - state[z][y].begin();
+		                shift = state[z][y].at(0);
+		                state[z][y].erase(state[z][y].begin()+0);
+		                state[z][y].push_back(shift);
+	                    }
+	                }
+	                sub_key = key_scheduler(sub_key);
+	                morph_cube(ctr, sub_key);
+                        ptxt.push_back(char(sub));
+                        ctr++;
+                    }
+		    if(!cin.eof()) {
+	                cout << ptxt;
+                        ptxt.clear();
+	            }
+		}
+            }
+       }
+    }
+};
